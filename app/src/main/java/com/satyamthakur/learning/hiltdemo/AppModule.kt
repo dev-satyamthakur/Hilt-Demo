4ccb7cec7986dff4eaf0f5d49143a0a827ee442b
Satyam Thakur <sat.thakur20@gmail.com>
2023-06-23 12:16:53 +0530

    Fixing Hilt gradle dependencies

--- app/src/main/java/com/satyamthakur/learning/hiltdemo/AppModule.kt
+++ app/src/main/java/com/satyamthakur/learning/hiltdemo/AppModule.kt
@@ -1,13 +1,16 @@
 package com.satyamthakur.learning.hiltdemo
 
+import android.app.Application
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.DefineComponent
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.internal.managers.ApplicationComponentManager
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponentManager::class)
+@InstallIn(ActivityComponent::class)
 object AppModule {
 
     @Singleton
--- app/src/main/java/com/satyamthakur/learning/hiltdemo/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/hiltdemo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.satyamthakur.learning.hiltdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var testString: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("myapplog", "Test String from MainActivity $testString")
+
     }
 }
