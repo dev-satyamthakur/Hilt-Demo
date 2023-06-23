73bd5c219da1750d987f6730c00504a65b9f822b
Satyam Thakur <sat.thakur20@gmail.com>
2023-06-23 12:21:07 +0530

    Using @Named annotation to inject

--- app/src/main/java/com/satyamthakur/learning/hiltdemo/AppModule.kt
+++ app/src/main/java/com/satyamthakur/learning/hiltdemo/AppModule.kt
@@ -1,20 +1,22 @@
 package com.satyamthakur.learning.hiltdemo
 
-import android.app.Application
 import dagger.Module
 import dagger.Provides
-import dagger.hilt.DefineComponent
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
 @InstallIn(ActivityComponent::class)
 object AppModule {
 
-    @Singleton
     @Provides
-    fun provideTestString() = "This is a string that will be injected"
+    @Named("String1")
+    fun provideTestString1() = "This is a string that will be injected"
+
+    @Provides
+    @Named("String2")
+    fun provideTestString2() = "This is another string that will be injected as well "
 
 }
